9a170dd52f72ec33a99762e2d747522b6c4f9c0e
Sunil Manthena <sunilvarma.manthena@jbhunt.com>
2022-03-04 19:07:48 -0600

    adding story book with scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Compiler, Component, Injector } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: [
+    `
+    @use 'sass:map';
+
+      h1{
+        color: red;
+        font-weight: map-get($weights , 'light' );
+      }
+
+
+    `
+  ],
+  // styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class AppComponent {
   title = 'jasmine-test';
+
+  constructor() {
+
+  }
+
 }
